f47aa12e42998b152e3d6dec7c6bdc3a62cfda9b
Chisom Daniel <chinwezechisom@gmail.com>
2023-12-14 12:50:47 +0100

    Fixed a lot of issues with the create user api endpoint.
    The endpoint works fine now, the password is being hashed when you create a new user using the `curl`
    commnd on the command line, but not when the admin dashbord is used and this is understandable
    seeing that the code to hash the password occur at the api endpoint and
    It seems like the admin dashboard isn't using this endpoint to create a new user.

--- back-end/api/rental_listing_api.py
+++ back-end/api/rental_listing_api.py
@@ -1,5 +1,5 @@
 # rental_listing_api.py
-from flask_restful import Resource
+from flask_restful import Resource, reqparse, abort
 from flask import jsonify
 from flasgger import swag_from
 from app.models import RentalListing
@@ -15,4 +15,23 @@ class RentalListingResource(Resource):
         
         rentals = RentalListing.query.all()
         return jsonify([rental.serialize for rental in rentals])
+    
+    def post(self):
+
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('name', type=str, required=True, help='Rental name required')
+            parser.add_argument('location', type=str, required=True, help='Location required')
+            parser.add_argument('min-rent', type=float, required=True, help='Minimum rent required')
+            parser.add_argument('max-rent', type=float, required=True, help='Maximum rent name required')
+            parser.add_argument('rented', type=bool, required=True, help='Rent status required')
+            parser.add_argument('owner_id', type=int, required=True, help='Owner ID required')
+
+            args = parser.parse_args()
+            new_rental = RentalListing()
+        except Exception as e:
+            return {
+                'error': str(e),
+                'message': 'Error creating listing'
+            }
 
